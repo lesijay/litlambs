3ece06388ae6a94e8a705e655c8f54ea23bd3f37
lesi sampson <lesisampson@gmail.com>
2023-07-11 14:39:45 +0000

    Update permissions.js
    included book access in permisions.js

--- ebooks/wai/permissions.js
+++ ebooks/wai/permissions.js
@@ -1,3 +1,17 @@
 if (document.referrer != "https://www.litlambs.org/ebooks/wai") {
   window.location = "https://www.litlambs.org/ebooks/wai";
 }
+
+
+                 // Book Access
+let route = window.location.href;
+let users = ["lesisampson@gmail.com"];
+
+function validate(form) {
+  var inputValue = form.inputbox.value;
+  if (users.includes(inputValue)) {
+    window.location = "./who-am-i.html";
+  } else {
+    alert("You do not yet have permissions to view this resource\nAlternatively, ensure you entered the correct email address");
+  }
+}
