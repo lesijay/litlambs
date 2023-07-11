7f4b791eddf36f33d5db77c123d12b21a3fbf5d4
lesi sampson <lesisampson@gmail.com>
2023-07-11 14:39:00 +0000

    Update app.js
    removed book access from app.js

--- app.js
+++ app.js
@@ -37,16 +37,4 @@ function clearForm() {
 }
 
 
-                 // Book Access
-let route = window.location.href;
-let users = ["lesisampson@gmail.com"];
-
-function validate(form) {
-  var inputValue = form.inputbox.value;
-  if (users.includes(inputValue)) {
-    window.location = "./who-am-i.html";
-  } else {
-    alert("You do not yet have permissions to view this resource\nAlternatively, ensure you entered the correct email address");
-  }
-}
 
