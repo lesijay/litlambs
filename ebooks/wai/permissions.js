12b023d1e75b87be1b83acd938eb2dbcaf3d3af0
lesi sampson <lesisampson@gmail.com>
2023-07-12 09:31:13 +0000

    Update permissions.js

--- ebooks/wai/permissions.js
+++ ebooks/wai/permissions.js
@@ -1,3 +1,5 @@
+
+// restrict book page to only users routed form validation form
 if (document.referrer != "https://www.litlambs.org/ebooks/wai/") {
   window.location = "https://www.litlambs.org/ebooks/wai/";
 }
@@ -8,10 +10,15 @@ let route = window.location.href;
 let users = ["lesisampson@gmail.com"];
 
 function validate(form) {
-  var inputValue = form.inputbox.value;
+  var inputValue = form.inputbox.value.toLowerCase();
   if (users.includes(inputValue)) {
     window.location = "./who-am-i.html";
   } else {
     alert("You do not yet have permissions to view this resource\nAlternatively, ensure you entered the correct email address");
   }
 }
+
+
+// setup date on the footer
+const date = (document.getElementById("date").innerHTML =
+  new Date().getFullYear());
