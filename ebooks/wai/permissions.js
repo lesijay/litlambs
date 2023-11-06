c2d1ce9feaea610f0d9c0da7db1461430beafb7f
lesi sampson <lesisampson@gmail.com>
2023-11-06 08:53:48 +0000

    Update permissions.js

--- ebooks/wai/permissions.js
+++ ebooks/wai/permissions.js
@@ -11,7 +11,7 @@ let users = ["lesisampson@gmail.com","annatimothyemeh@gmail.com","givtj1@gmail.c
              "blessingogua@gmail.com","stellakay88@gmail.com","miracleezinne28@gmail.com","lenonchihowa@gmail.com"
              ,"obriko.ejiro@gmail.com","veronicachuks2015@gmail.com","faithgifted17@gmail.com","okekelinda1409@gmail.com",
              "alexbetty2014@gmail.com","mbelaobe@outlook.com","moadeloye54@gmail.com","shalomosadebe@gmail.com","dikechi59@gmail.com",
-            "zoicbuz@gmail.com"];
+            "zoicbuz@gmail.com","fhayvor@gmail.com","chisomwami@gmail.com","obubamiracle@gmail.com"];
 
 function validate(form) {
   var inputValue = form.inputbox.value.toLowerCase();
