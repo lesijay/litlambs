86886ece3ff71b43e98da1a93ceb85e93db460db
lesi sampson <lesisampson@gmail.com>
2024-03-18 13:34:21 +0000

    Update permissions.js
    Free acess revoked

--- ebooks/wai/permissions.js
+++ ebooks/wai/permissions.js
@@ -7,12 +7,13 @@ if (document.referrer != "https://www.litlambs.org/ebooks/wai/") {
 
                  // Book Access
 let route = window.location.href;
-let users = ["lesisampson@gmail.com","annatimothyemeh@gmail.com","givtj1@gmail.com","adeola4j@yahoo.com",
-             "blessingogua@gmail.com","stellakay88@gmail.com","miracleezinne28@gmail.com","lenonchihowa@gmail.com"
-             ,"obriko.ejiro@gmail.com","veronicachuks2015@gmail.com","faithgifted17@gmail.com","okekelinda1409@gmail.com",
-             "alexbetty2014@gmail.com","mbelaobe@outlook.com","moadeloye54@gmail.com","shalomosadebe@gmail.com","dikechi59@gmail.com",
-            "zoicbuz@gmail.com","fhayvor@gmail.com","chisomwami@gmail.com","obubamiracle@gmail.com","emenikeezioma@gmail.com","priscaakpos@gmail.com",
-"euchariarufus17@gmail.com","orkolade@gmail.com","orkolade@gmail.com","estheradegoke83@gmail.com"];
+let users = ["lesisampson@gmail.com"]
+//let users = ["lesisampson@gmail.com","annatimothyemeh@gmail.com","givtj1@gmail.com","adeola4j@yahoo.com",
+       //      "blessingogua@gmail.com","stellakay88@gmail.com","miracleezinne28@gmail.com","lenonchihowa@gmail.com"
+         //    ,"obriko.ejiro@gmail.com","veronicachuks2015@gmail.com","faithgifted17@gmail.com","okekelinda1409@gmail.com",
+         //    "alexbetty2014@gmail.com","mbelaobe@outlook.com","moadeloye54@gmail.com","shalomosadebe@gmail.com","dikechi59@gmail.com",
+          //  "zoicbuz@gmail.com","fhayvor@gmail.com","chisomwami@gmail.com","obubamiracle@gmail.com","emenikeezioma@gmail.com","priscaakpos@gmail.com",
+//"euchariarufus17@gmail.com","orkolade@gmail.com","orkolade@gmail.com","estheradegoke83@gmail.com"];
 function validate(form) {
   var inputValue = form.inputbox.value.toLowerCase();
   if (users.includes(inputValue)) {
